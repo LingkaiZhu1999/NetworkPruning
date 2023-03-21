c25701f6b994dadafee093a774d7ded3cf2640cb
LingkaiZhu1999 <zhulingkai123@gmail.com>
2023-03-21 14:02:47 +0100

    update

--- main_proposed_class.py
+++ main_proposed_class.py
@@ -26,16 +26,16 @@ import random
 import matplotlib.pyplot as plt
 
 sns.set_style('darkgrid')
-
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 parser = argparse.ArgumentParser()
-parser.add_argument("--method", default="proposed", help="the proposed method")
+parser.add_argument("--method", default="proposed_prune_ratio_fixed", help="the proposed method")
 parser.add_argument("--lr", default=0.01, type=float, help="Learning rate")
 parser.add_argument("--batch_size", default=60, type=int)
 parser.add_argument("--start_prune_round", default=0, type=int)
 parser.add_argument("--train_epochs", default=200, type=int)
 parser.add_argument("--print_freq", default=1, type=int)
 parser.add_argument("--valid_freq", default=1, type=int)
-parser.add_argument("--early_stop", default=25, type=int)
+parser.add_argument("--early_stop", default=80, type=int)
 parser.add_argument("--resume", action="store_true")
 parser.add_argument("--prune_type", default="local", type=str, help="local | global")
 parser.add_argument("--device", default="cuda:0", type=str)
@@ -45,6 +45,7 @@ parser.add_argument("--initial_percent", default=100, type=float, help='percenta
 parser.add_argument("--prune_rounds", default=1, type=int, help="Pruning args.prune_roundss count")
 parser.add_argument("--prune_ratio", default=1, type=float, help="Prune ratio during train")
 parser.add_argument("--prune_prob", default=0.01, type=float, help="probability to prune during train")
+parser.add_argument("--target_ratio", default=2, type=float, )
 parser.add_argument("--optimizer", default="sgd", help="adam | sgd")
 parser.add_argument("--weight_decay", default=1.2e-3, type=float, help="weight decay for adam optim")
 parser.add_argument("--seed", default=1, type=int)
@@ -118,17 +119,24 @@ class Proposed_prune():
         else:
             print("\nWrong Model choice\n")
             exit()
+        self.parameters_to_prune = []
+        self.initial_num_weights = []
+        self.target_num_weight = []
+        for name, module in self.model.named_modules():
+            if isinstance(module, nn.Conv2d) or isinstance(module, nn.Linear):
+                self.parameters_to_prune.append((module, 'weight'))
+        self.parameters_to_prune = tuple(self.parameters_to_prune)
+
+        # self.total_num_weights = sum(p.numel() for name, p in self.model.named_parameters() if 'weight' in name)
+        # self.target_num_weights = args.target_ratio * self.total_num_weights
         if args.prune_type == 'local':
-            prune.random_unstructured(self.model.fc1, name='weight', amount=1.0-args.initial_percent*0.01)
-            prune.random_unstructured(self.model.fc2, name='weight', amount=1.0-args.initial_percent*0.01)
-            prune.random_unstructured(self.model.fc3, name='weight', amount=1.0-args.initial_percent*0.01)
+            # i = 0
+            for layer, name in self.parameters_to_prune:
+                # i += 1
+                # if i != len(self.parameters_to_prune):
+                prune.random_unstructured(layer, name=name, amount=1.0-args.initial_percent*0.01)
         elif args.prune_type == 'global':
-            parameters_to_prune = (
-                (self.model.fc1, 'weight'),
-                (self.model.fc2, 'weight'),
-                (self.model.fc3, 'weight')
-            )
-            prune.global_unstructured(parameters_to_prune, pruning_method=prune.RandomUnstructured, amount=1.0-args.initial_percent*0.01)
+            prune.global_unstructured(self.parameters_to_prune, pruning_method=prune.RandomUnstructured, amount=1.0-args.initial_percent*0.01)
         else:
             raise Exception('Invalid prune type.')
         self.save_path = f"{os.getcwd()}/saves/{args.method}/{args.dataset}/{args.arch_type}_lr_{args.lr}_{args.optimizer}_initial_percent_{args.initial_percent}/{args.dataset}/"
@@ -140,6 +148,12 @@ class Proposed_prune():
                 print('%s: %s' %(arg, getattr(args, arg)), file=f) 
     
     def prune(self, ):
+        # plot the histogram of the initial distribution
+        for name, p in self.model.named_parameters():
+            weight_nz = p[torch.nonzero(p, as_tuple=True)]
+            plt.hist(weight_nz.cpu().data.view(-1), bins=30)
+            plt.savefig(os.path.join(self.plot_path, f"{name}_initial.png"))
+            plt.close()
         writer = SummaryWriter(self.save_path)
         criterion = nn.CrossEntropyLoss()
         if args.optimizer == 'adam':
@@ -168,9 +182,9 @@ class Proposed_prune():
         comp[0] = comp1
         pbar = tqdm(range(args.train_epochs))
 
-        for train_iter in pbar:
+        for train_epoch in pbar:
             # Frequency for Testing
-            if train_iter % args.valid_freq == 0:
+            if train_epoch % args.valid_freq == 0:
                 val_accuracy = self.test(self.model, self.val_loader, criterion)
 
                 # Save Weights
@@ -182,31 +196,33 @@ class Proposed_prune():
             
             # Training
             loss = self.train(self.model, self.train_loader, optimizer, criterion, self.args)
-            torch.save(self.model, os.path.join(self.save_path, f"{train_iter}_model_{args.prune_type}.pt"))
-            all_loss[train_iter] = loss
-            all_accuracy[train_iter] = val_accuracy
+            torch.save(self.model, os.path.join(self.save_path, f"{train_epoch}_model_{args.prune_type}.pt"))
+            all_loss[train_epoch] = loss
+            all_accuracy[train_epoch] = val_accuracy
             # Frequency for Printing Accuracy and Loss
-            if train_iter % args.print_freq == 0:
+            if train_epoch % args.print_freq == 0:
                 pbar.set_description(
-                    f'Train Epoch: {train_iter}/{args.train_epochs} Loss: {loss:.6f} Val Accuracy: {val_accuracy:.2f}% Best Val Accuracy: {best_accuracy:.2f}%')       
+                    f'Train Epoch: {train_epoch}/{args.train_epochs} Loss: {loss:.6f} Val Accuracy: {val_accuracy:.2f}% Best Val Accuracy: {best_accuracy:.2f}%')       
             if early_stop_trigger > args.early_stop:
                 break
 
-            comp1 = utils.print_nonzeros(self.model, writer, train_iter)
-            sparsity_[train_iter] = round(100.0-comp1, 1)
-            # best_val_model = torch.load(os.path.join(self.save_path, f"{train_iter}_model_{args.prune_type}.pt"))
+            comp1 = utils.print_nonzeros(self.model, writer, train_epoch)
+            sparsity_[train_epoch] = round(100.0-comp1, 1)
+            # best_val_model = torch.load(os.path.join(self.save_path, f"{train_epoch}_model_{args.prune_type}.pt"))
             test_accuracy = self.test(self.model, self.test_loader, criterion)
             print(f'Test Accuracy: {test_accuracy}')
-            writer.add_scalar('Accuracy_sparsity/val', best_accuracy, sparsity)
-            writer.add_scalar('Accuracy_sparsity/test', test_accuracy, sparsity)
-            writer.add_scalar('Accuracy_epoch/test', test_accuracy, train_iter)
+            writer.add_scalar('Accuracy_sparsity/val', best_accuracy, round(100.0-comp1, 1))
+            writer.add_scalar('Accuracy_sparsity/test', test_accuracy, round(100.0-comp1, 1))
+            writer.add_scalar('Accuracy_epoch/test', test_accuracy, train_epoch)
             bestacc[0] = best_accuracy
-            testacc[train_iter] = test_accuracy
-            fig = utils.plot_sparsity_testacc(sparsity_[:train_iter+1], testacc[:train_iter+1], self.plot_path)
-            writer.add_figure('sparsity_testacc', fig, train_iter)
+            testacc[train_epoch] = test_accuracy
+            fig = utils.plot_sparsity_testacc(sparsity_[:train_epoch+1], testacc[:train_epoch+1], self.plot_path)
+            writer.add_figure('sparsity_testacc', fig, train_epoch)
+            if comp1 < 2.: 
+                break
         for name, p in self.model.named_parameters():
             weight_nz = p[torch.nonzero(p, as_tuple=True)]
-            plt.hist(weight_nz.cpu().data.view(-1), bins=20)
+            plt.hist(weight_nz.cpu().data.view(-1), bins=30)
             plt.savefig(os.path.join(self.plot_path, f"{name}.png"))
             plt.close()
 
@@ -233,7 +249,8 @@ class Proposed_prune():
                     p.grad.data = grad_tensor.to(self.device)
 
             optimizer.step()
-            self.prune_and_reconnect(model, args.prune_prob, args.prune_ratio * 0.01)
+            if batch_idx % 100 == 0:
+                self.prune_and_reconnect(model, args.prune_prob, args.prune_ratio * 0.01)
         
         return metric.compute().item()
 
@@ -253,34 +270,41 @@ class Proposed_prune():
         return accuracy
     
     def prune_and_reconnect(self, model, prune_prob, prune_ratio):
-        if random.uniform(0, 1) < prune_prob:
-            prune_count = []
-            mask_bef_pru = []
-            for name, param in model.named_buffers():
-                prune_count.append(int(np.rint((torch.count_nonzero(param.data)).cpu().numpy() * prune_ratio)))
-                mask_bef_pru.append(param.data)
-            # prune the connections
-            prune.l1_unstructured(model.fc1, name='weight', amount=prune_ratio)
-            prune.l1_unstructured(model.fc2, name='weight', amount=prune_ratio)
-            # if prune_count[2] / 2 > 1:
-            prune.l1_unstructured(model.fc3, name='weight', amount=prune_ratio/2)
-            # else:
-                # prune.l1_unstructured(model.fc3, name='weight', amount=1)
-            # reconnect the zeroed connections
+        prune_count = []
+        mask_bef_pru = []
+        for name, param in model.named_buffers():
+            prune_count.append(int(np.rint((torch.count_nonzero(param.data)).cpu().numpy() * prune_ratio)))
+            mask_bef_pru.append(param.data)
+        # prune the connections
+        if args.prune_type == 'local':
             i = 0
-            for module in list(model.children()):
-                mask = mask_bef_pru[i]
-                if i == 2:
-                    num_reconnect = prune_count[i] // 4 
+            for layer, name in self.parameters_to_prune:
+                i +=1
+                if i == len(self.parameters_to_prune):
+                    prune.l1_unstructured(layer, name=name, amount=prune_ratio/2)
                 else:
-                    num_reconnect = prune_count[i] // 2 # add less connection than prune
-                zero_indices = (mask.view(-1) == 0).nonzero()[0:num_reconnect]
-            #     # shuffle it
-                idx = torch.randperm(zero_indices.nelement())
-                zero_indices = zero_indices.view(-1)[idx].view(zero_indices.size())[0:num_reconnect]
-        
-                (list(module.named_buffers())[0][1]).view(-1)[zero_indices] = 1.
-                i += 1
+                    # prune at half ratio for the last output layer
+                    prune.l1_unstructured(layer, name=name, amount=prune_ratio)
+        elif args.prune_type == 'global':
+            prune.global_unstructured(self.parameters_to_prune, pruning_method=prune.L1Unstructured, amount=self.prune_percent)
+        # if prune_count[2] / 2 > 1:
+        # else:
+            # prune.l1_unstructured(model.fc3, name='weight', amount=1)
+        # reconnect the zeroed connections
+        i = 0
+        for module in list(model.children()):
+            mask = mask_bef_pru[i]
+            if i == len(self.parameters_to_prune) - 1:
+                num_reconnect = prune_count[i] // 4 
+            else:
+                num_reconnect = prune_count[i] // 2 # add less connection than prune
+            zero_indices = (mask.view(-1) == 0).nonzero()
+        #     # shuffle it
+            idx = torch.randperm(zero_indices.nelement())
+            zero_indices = zero_indices.view(-1)[idx].view(zero_indices.size())[0:num_reconnect]
+    
+            (list(module.named_buffers())[0][1]).view(-1)[zero_indices] = 1.
+            i += 1
 
 def main():
     proposed_prune = Proposed_prune(args)
